d85f666bf6f50c4b2ded1c30b6fe22de7c5b6655
lysevi <lysevi@gmail.com>
2019-03-20 11:43:59 +0300

    abstract_consumer: snapshot.

--- librft/consensus.h
+++ librft/consensus.h
@@ -17,6 +17,7 @@ class abstract_consensus_consumer {
 public:
   virtual void apply_cmd(const command &cmd) = 0;
   virtual void reset() = 0;
+  virtual command snapshot() = 0;
 };
 
 class consensus {
