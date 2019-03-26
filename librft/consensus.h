832c62fd978f5d434367023ce24328c22c47f692
lysevi <lysevi@gmail.com>
2019-03-26 11:51:39 +0300

    thread safety.

--- librft/consensus.h
+++ librft/consensus.h
@@ -34,7 +34,10 @@ public:
                    abstract_cluster *cluster,
                    const logdb::journal_ptr &jrn,
                    abstract_consensus_consumer *consumer);
-  node_state_t state() const { return _state; }
+  node_state_t state() const {
+    std::lock_guard<std::mutex> l(_locker);
+    return _state;
+  }
   node_state_t &rw_state() { return _state; }
 
   NODE_KIND kind() const { return _state.node_kind; }
