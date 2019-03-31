fd20d8d2f8d4e6122fc8329b773fac69a51bd818
lysevi <lysevi@gmail.com>
2019-03-31 19:39:01 +0300

    refactoring.

--- librft/queries.cpp
+++ librft/queries.cpp
@@ -5,13 +5,35 @@
 
 using namespace rft::queries;
 
-query_connect_t::query_connect_t(const dialler::message_ptr &msg) {
-  ENSURE(msg->get_header()->kind
-         == (dialler::message::kind_t)queries::QUERY_KIND::CONNECT);
+namespace {
+msgpack::unpacker get_unpacker(const dialler::message_ptr &msg) {
   msgpack::unpacker pac;
   pac.reserve_buffer(msg->size());
   memcpy(pac.buffer(), msg->value(), msg->size());
   pac.buffer_consumed(msg->size());
+  return pac;
+}
+
+template <typename... Args>
+dialler::message_ptr pack_to_message(rft::queries::QUERY_KIND kind, Args &&... args) {
+  msgpack::sbuffer buffer;
+  msgpack::packer<msgpack::sbuffer> pk(&buffer);
+  (pk.pack(args), ...);
+
+  auto needed_size = (dialler::message::size_t)buffer.size();
+  auto nd
+      = std::make_shared<dialler::message>(needed_size, (dialler::message::kind_t)kind);
+
+  memcpy(nd->value(), buffer.data(), buffer.size());
+  return nd;
+}
+
+} // namespace
+
+query_connect_t::query_connect_t(const dialler::message_ptr &msg) {
+  ENSURE(msg->get_header()->kind
+         == (dialler::message::kind_t)queries::QUERY_KIND::CONNECT);
+  msgpack::unpacker pac = get_unpacker(msg);
   msgpack::object_handle oh;
 
   pac.next(oh);
@@ -21,26 +43,13 @@ query_connect_t::query_connect_t(const dialler::message_ptr &msg) {
 }
 
 dialler::message_ptr query_connect_t::query_connect_t::to_message() const {
-  msgpack::sbuffer buffer;
-  msgpack::packer<msgpack::sbuffer> pk(&buffer);
-  pk.pack(protocol_version);
-  pk.pack(node_id);
-
-  auto needed_size = (dialler::message::size_t)buffer.size();
-  auto nd = std::make_shared<dialler::message>(
-      needed_size, (dialler::message::kind_t)QUERY_KIND::CONNECT);
-
-  memcpy(nd->value(), buffer.data(), buffer.size());
-  return nd;
+  return pack_to_message(queries::QUERY_KIND::CONNECT, protocol_version, node_id);
 }
 
 connection_error_t::connection_error_t(const dialler::message_ptr &mptr) {
   ENSURE(mptr->get_header()->kind
          == (dialler::message::kind_t)queries::QUERY_KIND::CONNECTION_ERROR);
-  msgpack::unpacker pac;
-  pac.reserve_buffer(mptr->size());
-  memcpy(pac.buffer(), mptr->value(), mptr->size());
-  pac.buffer_consumed(mptr->size());
+  msgpack::unpacker pac = get_unpacker(mptr);
   msgpack::object_handle oh;
 
   pac.next(oh);
@@ -50,26 +59,13 @@ connection_error_t::connection_error_t(const dialler::message_ptr &mptr) {
 }
 
 dialler::message_ptr connection_error_t::to_message() const {
-  msgpack::sbuffer buffer;
-  msgpack::packer<msgpack::sbuffer> pk(&buffer);
-  pk.pack(protocol_version);
-  pk.pack(msg);
-
-  auto needed_size = (dialler::message::size_t)buffer.size();
-  auto nd = std::make_shared<dialler::message>(
-      needed_size, (dialler::message::kind_t)QUERY_KIND::CONNECTION_ERROR);
-
-  memcpy(nd->value(), buffer.data(), buffer.size());
-  return nd;
+  return pack_to_message(queries::QUERY_KIND::CONNECTION_ERROR, protocol_version, msg);
 }
 
 command_t::command_t(const dialler::message_ptr &mptr) {
   ENSURE(mptr->get_header()->kind
          == (dialler::message::kind_t)queries::QUERY_KIND::COMMAND);
-  msgpack::unpacker pac;
-  pac.reserve_buffer(mptr->size());
-  memcpy(pac.buffer(), mptr->value(), mptr->size());
-  pac.buffer_consumed(mptr->size());
+  msgpack::unpacker pac = get_unpacker(mptr);
   msgpack::object_handle oh;
 
   pac.next(oh);
@@ -78,14 +74,5 @@ command_t::command_t(const dialler::message_ptr &mptr) {
 }
 
 dialler::message_ptr command_t::to_message() const {
-  msgpack::sbuffer buffer;
-  msgpack::packer<msgpack::sbuffer> pk(&buffer);
-  pk.pack(cmd.to_byte_array());
-
-  auto needed_size = (dialler::message::size_t)buffer.size();
-  auto nd = std::make_shared<dialler::message>(
-      needed_size, (dialler::message::kind_t)QUERY_KIND::COMMAND);
-
-  memcpy(nd->value(), buffer.data(), buffer.size());
-  return nd;
+  return pack_to_message(queries::QUERY_KIND::COMMAND, cmd.to_byte_array());
 }
