2d8d31c7dea04f995d00ce7500256b2b28778a17
lysevi <lysevi@gmail.com>
2019-04-02 16:31:56 +0300

    utils::strings.

--- libutils/strings.h
+++ libutils/strings.h
@@ -17,11 +17,22 @@ using std::to_string;
 EXPORT std::string to_string(const char *_Val);
 EXPORT std::string to_string(const std::string &_Val);
 
-template <size_t N, class T>
-void args_as_string(std::string (&s)[N], size_t pos, size_t &sz, T &&arg) noexcept {
-  auto str = to_string(std::forward<T>(arg));
+template <size_t N, class Head>
+void args_as_string(std::string (&s)[N], size_t pos, size_t &sz, Head &&head) noexcept {
+  auto str = to_string(std::forward<Head>(head));
   sz += str.size();
   s[pos] = std::move(str);
+}
+template <size_t N, class Head, class... Tail>
+void args_as_string(std::string (&s)[N],
+                    size_t pos,
+                    size_t &sz,
+                    Head &&head,
+                    Tail &&... tail) noexcept {
+  auto str = to_string(std::forward<Head>(head));
+  sz += str.size();
+  s[pos] = std::move(str);
+  args_as_string(s, pos + 1, sz, std::forward<Tail>(tail)...);
 }
 } // namespace inner
 
@@ -30,7 +41,7 @@ std::string args_to_string(Args &&... args) noexcept {
   const size_t n = sizeof...(args);
   std::string ss[n];
   size_t sz = 0;
-  (inner::args_as_string(ss, size_t(0), sz, std::forward<Args>(args)), ...);
+  inner::args_as_string(ss, size_t(0), sz, std::forward<Args>(args)...);
   std::string result;
   result.reserve(sz);
   for (auto &&v : ss) {
