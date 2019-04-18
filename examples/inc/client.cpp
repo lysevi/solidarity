3fb9eb15c9286afaba6ec29065934aa06966b8a5
lysevi <lysevi@gmail.com>
2019-04-18 16:38:45 +0300

    client_inc: try..catch.

--- examples/inc/client.cpp
+++ examples/inc/client.cpp
@@ -59,47 +59,55 @@ int main(int argc, char **argv) {
   }
 
   while (true) {
-    std::cout << "write i:" << i << std::endl;
-    std::mutex locker;
-    std::unique_lock ulock(locker);
-    bool is_on_update_received = false;
-    std::condition_variable cond;
-    bool is_network_error = false;
-    auto uh_id = c->add_event_handler([&is_on_update_received, &cond, &is_network_error](
-                                          const solidarity::client_event_t ev) {
-      if (ev.kind == solidarity::client_event_t::event_kind::NETWORK) {
-        if (ev.net_ev.value().ecode != solidarity::ERROR_CODE::OK) {
-          is_network_error = true;
+    try {
+      std::cout << "write i:" << i << std::endl;
+      std::mutex locker;
+      std::unique_lock ulock(locker);
+      bool is_on_update_received = false;
+      std::condition_variable cond;
+      bool is_network_error = false;
+      auto uh_id
+          = c->add_event_handler([&is_on_update_received, &cond, &is_network_error](
+                                     const solidarity::client_event_t ev) {
+              if (ev.kind == solidarity::client_event_t::event_kind::NETWORK) {
+                if (ev.net_ev.value().ecode != solidarity::ERROR_CODE::OK) {
+                  is_network_error = true;
+                }
+              }
+              std::cerr << solidarity::to_string(ev) << std::endl;
+              is_on_update_received = true;
+              cond.notify_all();
+            });
+
+      solidarity::utils::elapsed_time el;
+
+      auto res = c->send(common_inner::int2cmd(i).data);
+      std::cout << solidarity::utils::strings::args_to_string("res: ", res) << std::endl;
+      if (res != solidarity::ERROR_CODE::OK) {
+        c->rm_event_handler(uh_id);
+        continue;
+      }
+      while (true) {
+        cond.wait(ulock, [&is_on_update_received]() { return is_on_update_received; });
+        if (is_on_update_received) {
+          break;
         }
       }
-      std::cerr << solidarity::to_string(ev) << std::endl;
-      is_on_update_received = true;
-      cond.notify_all();
-    });
 
-    solidarity::utils::elapsed_time el;
+      auto answer = c->read({});
+      std::cout << "elapsed: " << el.elapsed() << std::endl;
+      std::cout << "sz: " << answer.size() << std::endl;
 
-    auto res = c->send(common_inner::int2cmd(i).data);
-    std::cout << solidarity::utils::strings::args_to_string("res: ", res) << std::endl;
-    if (res != solidarity::ERROR_CODE::OK) {
-      continue;
-    }
-    while (true) {
-      cond.wait(ulock, [&is_on_update_received]() { return is_on_update_received; });
-      if (is_on_update_received) {
+      c->rm_event_handler(uh_id);
+
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      ++i;
+      if (is_network_error) {
         break;
       }
-    }
-    auto answer = c->read({});
-    std::cout << "elapsed: " << el.elapsed() << std::endl;
-    std::cout << "sz: " << answer.size() << std::endl;
-
-    c->rm_event_handler(uh_id);
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    ++i;
-    if (is_network_error) {
-      break;
+    } catch (solidarity::exception &e) {
+      std::cerr << e.what() << std::endl;
+      return 0;
     }
   }
 }
